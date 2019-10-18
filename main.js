f7c01ed56ad8ed2cf2130dfabdfc10785379e6f8
Nabin Paudyal <nabindenepali@outlook.com>
2019-10-18 14:05:29 +0545

    Add collision between paddle and ball

--- main.js
+++ main.js
@@ -41,13 +41,33 @@ function Paddle(id, x, minX, maxX, speed) {
   this.render = function() {
     _element.style.left = _x + 'px';
     _element.style.bottom = _y + 'px';
-  }
+  };
+
+  /**
+   * Function for initial release of the ball
+   * @param ball
+   */
+  this.releaseBall = function(ball) {
+    if (ball.getXCenter() < _x || ball.getXCenter() > _x + 100) {
+      ball.setVelocity({x: 0, y: -3});
+    } else {
+      ball.setVelocity({x: 3, y: 3});
+    }
+  };
+
+  /**
+   * Function to check collision between paddle and ball
+   * @param ball
+   */
+  this.strikesBall = function(ball) {
+    return ball.getXCenter() > _x && ball.getXCenter() < _x + 100 && ball.getY() === 11;
+  };
 }
 
 /**
  * Create a new paddle with desired properties and render
  */
-paddle = new Paddle('paddle', 451, 1, 901, 100);
+paddle = new Paddle('paddle', 451, 1, 901, 10);
 paddle.render();
 
 /**
@@ -137,14 +157,20 @@ function Ball(id, x, y, minX, maxX, minY, maxY, velocity) {
       _velocity.x = -_velocity.x;
     }
     _y += _velocity.y;
-    if (_y < _minY) { // Prevent move to the top if the ball is already on the top edge
-      _y = _minY;
-      _velocity.y = -_velocity.y;
+    if (_y < _minY) { // Hide the ball if it moves to the floor
+      _element.style.display = 'none';
     }
-    if (_y > _maxY) { // Prevent move to the bottom if the ball is already on the bottom edge
+    if (_y > _maxY) { // Prevent move to the bottom if the ball is already on the top edge
       _y = _maxY;
       _velocity.y = -_velocity.y;
     }
+  };
+
+  /**
+   * Change velocity to simulate reflection along y-axis
+   */
+  this.reflectY = function() {
+    _velocity.y = -_velocity.y;
   };
 
   /**
@@ -153,17 +179,67 @@ function Ball(id, x, y, minX, maxX, minY, maxY, velocity) {
   this.render = function() {
     _element.style.left = _x + 'px';
     _element.style.bottom = _y + 'px';
+  };
+
+  /**
+   * Get x cordinates of the center of the ball
+   */
+  this.getXCenter = function() {
+    return _x + 10;
+  };
+
+  /**
+   * Get y cordinates of the bottom of the ball
+   */
+  this.getY = function() {
+    return _y;
+  };
+
+  /**
+   * Function to set velocity of the ball
+   */
+  this.setVelocity = function(velocity) {
+    _velocity = velocity;
+  };
+
+  /***
+   * Check if the ball has already dropped on the floor
+   */
+  this.dropped = function() {
+    return _y < 1;
   }
 }
 
 /**
  * Create a new ball with desired properties and render
  */
-ball = new Ball('ball', 491, 11, 1, 981, 1, 581, {x: 10, y: 10});
+ball = new Ball('ball', 491, 11, 1, 981, 1, 581, {x: 0, y: 0});
 ball.render();
 
-setInterval(function () {
-  ball.move();
-  ball.render();
-}, 100);
+/**
+ * Strike the ball and compute velocity when space is pressed
+ */
+document.addEventListener('keypress', function(e) {
+  if (e.key === ' ') {
+    game();
+  }
+});
+
+/***
+ * Main logic of the game runs here
+ */
+function game() {
+  paddle.releaseBall(ball);
+  const intervalId = setInterval(function() {
+    ball.move();
+    ball.render();
+    if (paddle.strikesBall(ball)) {
+      ball.reflectY();
+    }
+    if (ball.dropped()) {
+      clearInterval(intervalId);
+    }
+  }, 10);
+}
+
 
